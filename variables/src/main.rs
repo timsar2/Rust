389485826b213535b3241c421860f74d1d48d68c
timsar2 <info.golab@gmail.com>
2018-11-06 14:33:29 +0330

    tuple point index

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     println!("The value of x is: {}", x);
 
     let tup = (500,6.4,1);
-    let (c,z,heart_eyed_cat) = tup;
+    let c = tup.0;
+    let z = tup.1;
+    let heart_eyed_cat = tup.2;
     println!("The value of guess is: {}, {}, {}", c,z,heart_eyed_cat);
 }
