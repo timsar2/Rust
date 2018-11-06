38e8de12feafb056f72d6d7696cc2efdce384a94
timsar2 <info.golab@gmail.com>
2018-11-06 16:21:07 +0330

    fibonachi

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-    for element in a.iter(){
-        println!("element is {}", element);
+    const X: i32 = 6;
+    let fibonachi = fibo_rust(X);
+    println!("fib {} is {}", X, fibonachi);
+}
+
+fn fibo_rust(x: i32) -> i32 {
+    let mut fib: [i32; 3] = [0, -1, 1];
+    for i in 0..x {
+        fib[0] = fib[1];
+        fib[1] = fib[2];
+        fib[2] = fib[0] + fib[1];
+        println!("{}: {} ",i ,fib[2]);
     }
-    for number in (1..4).rev() {
-        println!("number is {}", number);
-    }
+    fib[2]
 }
