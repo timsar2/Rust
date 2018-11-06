fc20a94f9fa633b37003ac98f31e8485693896e5
timsar2 <info.golab@gmail.com>
2018-11-06 15:18:30 +0330

    fn define

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,15 +1,7 @@
 fn main() {
-    let x = 5;
+    another_function(5)
+}
 
-    let x = x + 1;
-
-    let x = x * 2;
-
-    println!("The value of x is: {}", x);
-
-    let tup = (500,6.4,1);
-    let c = tup.0;
-    let z = tup.1;
-    let heart_eyed_cat = tup.2;
-    println!("The value of guess is: {}, {}, {}", c,z,heart_eyed_cat);
+fn another_function( x: i32){
+    println!("value x is {}", x);
 }
