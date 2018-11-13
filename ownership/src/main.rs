a45279b7d0caf4ca06a693f0e0073d5339f78559
timsar2 <info.golab@gmail.com>
2018-11-13 14:44:26 +0330

    refrence

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    refrence();
+
     let x = 5;
     makes_copy(x);
     
@@ -9,6 +11,15 @@ fn main() {
     let s2 = String::from("re take ownership");
     let (s3, len) = calculate_lenght(s2);
     println!("{}, sie:{}", s3, len);
+}
+
+fn refrence() {
+    let s1 = String::from("hello");    
+    let len = refrence_calc_length(&s1);
+    println!("refrence: {}", len);
+}
+fn refrence_calc_length(s: &String) -> usize {
+    s.len()
 }
 
 fn makes_copy(some_integer: i32) {
