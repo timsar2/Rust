cea83803ea03a392a9de952bfc7edf22e631eece
Mrg <info.golab@gmail.com>
2018-12-01 15:01:11 +0330

    calc rectangles

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let withd1 = 30;
+    let height1 = 30;
+
+    println!(
+        "The area of rectangles is {} square pixels",
+        area(withd1, height1)
+    );
+}
+
+fn area(withd: u32, height: u32) -> u32 {
+    withd * height
 }
