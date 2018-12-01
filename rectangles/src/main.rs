72c9205cb77b22d75e89ad2e7641c87898a67594
Mrg <info.golab@gmail.com>
2018-12-01 19:10:59 +0330

    traits

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,14 +1,14 @@
-struct Rectangle {
-    width: u32,
-    height: u32
-}
-
 fn main() {
     let rect2 = Rectangle { width: 40, height: 60 };
 
     println!(
         "The area of rectangles is {} square pixels",
         area_struct_tup(&rect2)
+    );
+
+    println!(
+        "The area of rectangles is {:#?} square pixels",
+        rect2
     );
 
     let rect1 = (30, 50);
@@ -25,6 +25,11 @@ fn main() {
         "The area of rectangles is {} square pixels",
         area(width1, height1)
     );
+}
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
 }
 
 fn area_struct_tup(rectangle: &Rectangle) -> u32{
